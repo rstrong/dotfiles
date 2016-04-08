d440d5b6ab4040a3b2970088ae654b18367407e2
Rick Strong <rjstrong@gmail.com>
2016-04-08 18:17:46 -0400

    fixing install script for vim dir

--- install.pl
+++ install.pl
@@ -3,12 +3,20 @@
 use strict; use warnings;
 
 my @rc_files = qw/bashrc vimrc screenrc/;
+my @dirs = qw/vim/;
 
 sub backup_file
 {
   my $file = shift;
   print "$file exists, backing up a copy...\n";
   system("cp $file $file." . time() . ".back");
+}
+
+sub mv_dir
+{
+    my $dir = shift;
+    print "directory $dir exists, moving to $dir.back\n";
+    system("mv $dir $dir.back");
 }
 
 foreach my $rc_file (@rc_files)
@@ -20,4 +28,13 @@ foreach my $rc_file (@rc_files)
       backup_file($fn);
     }
     `ln -sf ~/.dotfiles/$rc_file ~/.$rc_file`
+}
+
+foreach my $dir (@dirs)
+{
+    my $dn = "\$HOME/\.$dir";
+    if (-e $dn and -d $dn) {
+        mv_dir($dn);
+    }
+    system("mkdir $dn && cp -r $dir $dn");
 }
